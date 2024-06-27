fd4d085b813227af15c7325be5d45c8735457f7e
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-06-27 11:14:27 +0300

    Done for 1st tenants , sell and and sell home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -36,6 +36,55 @@ const Home = () => {
           </Link>
         </div>
       )}
+
+{view === 'tenant' && (
+        <div style={styles.tenantSection}>
+          <h1 style={styles.heading}>Tenants
+          </h1>
+          <p style={styles.paragraph}>The Property owners will be able to get rid of third party fees such as house agents which
+            sometimes become to pricey for them. The Property owners will be able to log into and download Mobile App, sign up, fill in their
+            details upload photos together with its locations , Pay and Launch their products.
+            An introductory fee of sh 5000/- will be charged per month for the Property owners to advertise and reach wide range of clients 
+            and customers all across 47 counties. This equates to only sh 167/- per day. In most instances once settles for a property, the 
+            agents charge the property owners upto 50% of the total house rent. <b>Msamo Properties App is here to relieve the property owners of huge charges by agents and Brokers</b>
+          </p>
+          <Link to="/signup">
+            <button style={styles.button}>Create Account</button>
+          </Link>
+        </div>
+      )}
+
+{view === 'sellLand' && (
+        <div style={styles.sellLandSection}>
+          <h1 style={styles.heading}>Sell Land</h1>
+          <p style={styles.paragraph}>The Property owners will be able to get rid of third party fees such as house agents which
+            sometimes become to pricey for them. The Property owners will be able to log into and download Mobile App, sign up, fill in their
+            details upload photos together with its locations , Pay and Launch their products.
+            An introductory fee of sh 5000/- will be charged per month for the Property owners to advertise and reach wide range of clients 
+            and customers all across 47 counties. This equates to only sh 167/- per day. In most instances once settles for a property, the 
+            agents charge the property owners upto 50% of the total house rent. <b>Msamo Properties App is here to relieve the property owners of huge charges by agents and Brokers</b>
+          </p>
+          <Link to="/signup">
+            <button style={styles.button}>Create Account</button>
+          </Link>
+        </div>
+      )}
+
+{view === 'sellHome' && (
+        <div style={styles.sellHomeSection}>
+          <h1 style={styles.heading}>Sell Home</h1>
+          <p style={styles.paragraph}>The Property owners will be able to get rid of third party fees such as house agents which
+            sometimes become to pricey for them. The Property owners will be able to log into and download Mobile App, sign up, fill in their
+            details upload photos together with its locations , Pay and Launch their products.
+            An introductory fee of sh 5000/- will be charged per month for the Property owners to advertise and reach wide range of clients 
+            and customers all across 47 counties. This equates to only sh 167/- per day. In most instances once settles for a property, the 
+            agents charge the property owners upto 50% of the total house rent. <b>Msamo Properties App is here to relieve the property owners of huge charges by agents and Brokers</b>
+          </p>
+          <Link to="/signup">
+            <button style={styles.button}>Create Account</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
