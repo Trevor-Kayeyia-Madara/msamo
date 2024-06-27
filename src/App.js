d6beb564773b1a89acdb0ae05f7c486536bd161c
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-06-27 17:53:09 +0300

    I have done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/home';
-import Loading from './components/Home/Loading';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
+import Home from './Pages/index';
+
 
 const App = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Loading />} />
-          <Route  path='/home' element={<Home />} />
-          <Route path='/sign-up' element={<SignUp />} />
-          <Route path='/login' element={<Login />} />
+        <Route path='/' component={<Home />} />
         </Routes>
       </div>
     </Router>
