037bd1082ab31bafd62fcf4c1bfcc57d7ac2d6db
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-07-01 15:43:52 +0300

    Done with front end Pay Now

--- src/Pages/Payment/Pay.js
+++ src/Pages/Payment/Pay.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './paynow.css';
 
 function Pay() {
   return (
-    <div>Pay</div>
+    <div className="pay-now">
+    <h2>Complete Payment</h2>
+    <p>Please use the following details to complete your payment:</p>
+    <ul>
+      <li><strong>Paybill Number:</strong> 123456</li>
+      <li><strong>Account Number:</strong> Your phone number</li>
+      <li><strong>Amount:</strong> Enter the amount as agreed</li>
+    </ul>
+    <p>Once payment is completed, you will receive a confirmation message.</p>
+  </div>
   )
 }
 
