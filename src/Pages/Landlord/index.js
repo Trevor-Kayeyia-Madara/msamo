d2943e4356f5a81035183f3fe9b622f785de7405
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-07-01 10:54:57 +0300

    I have corrected API endpoint

--- src/Pages/Landlord/index.js
+++ src/Pages/Landlord/index.js
@@ -74,7 +74,7 @@ const counties = [
       });
   
       try {
-        const response = await axios.post('https://msamo-backend.onrender.com/landlord', formData, {
+        const response = await axios.post('https://msamo-backend.onrender.com/properties', formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
